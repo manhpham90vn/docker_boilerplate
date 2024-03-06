1f9064b33de6c7eed9e2cce183135aad7be00393
Manh Pham <manhpham90vn@icloud.com>
2024-03-06 09:24:00 +0700

    sync

--- bootstrap-ubuntu.sh
+++ bootstrap-ubuntu.sh
@@ -39,22 +39,17 @@ ln -s "$(pwd)/.gitconfig" "$HOME/.gitconfig"
 
 # install docker
 bash $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/scripts/info.sh "Install docker"
-# Add Docker's official GPG key:
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-# Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-# Install docker-compose
 sudo curl -SL https://github.com/docker/compose/releases/download/v2.24.6/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
@@ -62,9 +57,6 @@ chmod +x /usr/local/bin/docker-compose
 # install pyenv
 bash $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/scripts/info.sh "Install pyenv"
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-
-# install pyenv dependencies
-bash $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/scripts/info.sh "Install pyenv dependencies"
 sudo apt update
 sudo apt install -y build-essential libssl-dev zlib1g-dev \
     libbz2-dev libreadline-dev libsqlite3-dev curl \
@@ -92,7 +84,18 @@ chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
 # install helm
-# install ansible
+bash $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/scripts/info.sh "Install helm"
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install -y helm
+
+# install 
+bash $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/scripts/info.sh "Install ansible"
+wget -O- "https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=get&search=0x6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367" | sudo gpg --dearmour -o /usr/share/keyrings/ansible-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/ansible-archive-keyring.gpg] http://ppa.launchpad.net/ansible/ansible/ubuntu jammy main" | sudo tee /etc/apt/sources.list.d/ansible.list
+sudo apt update && sudo apt install -y ansible
 # install aws cli
 # install sam cli
 # install eb cli
@@ -109,4 +112,5 @@ sudo mv ./kubectl /usr/local/bin/kubectl
 sudo usermod -aG docker $USER
 newgrp docker
 
+# change shell to zsh
 chsh -s $(which zsh)
