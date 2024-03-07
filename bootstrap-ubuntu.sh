070ab1fc972e3c0119aa3110cfa9c24b2e4d4f0b
Manh Pham <manhpham90vn@icloud.com>
2024-03-07 08:20:42 +0700

    sync

--- bootstrap-ubuntu.sh
+++ bootstrap-ubuntu.sh
@@ -112,6 +112,24 @@ sudo apt update && sudo apt install -y ansible
 # install postgres
 # install redis
 # install mongodb
+# install vagrant
+# install minikube
+
+# install virtualbox
+# install datagrip
+# install vscode
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install -y apt-transport-https
+sudo apt update
+sudo apt install -y code
+
+# install chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P /tmp
+sudo apt install /tmp/google-chrome-stable_current_amd64.deb
 
 # Docker post-installation
 sudo usermod -aG docker $USER
