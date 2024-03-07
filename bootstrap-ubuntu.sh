f3a2c83def0a0be72522bc8c6b06093804dddf9a
Manh Pham <manhpham90vn@icloud.com>
2024-03-07 08:06:16 +0700

    sync

--- bootstrap-ubuntu.sh
+++ bootstrap-ubuntu.sh
@@ -95,11 +95,12 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.
 sudo apt-get update
 sudo apt-get install -y helm
 
-# install 
+# install ansible
 bash $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/scripts/info.sh "Install ansible"
 wget -O- "https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=get&search=0x6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367" | sudo gpg --dearmour -o /usr/share/keyrings/ansible-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/ansible-archive-keyring.gpg] http://ppa.launchpad.net/ansible/ansible/ubuntu jammy main" | sudo tee /etc/apt/sources.list.d/ansible.list
 sudo apt update && sudo apt install -y ansible
+
 # install aws cli
 # install sam cli
 # install eb cli
