7306965d3df3ba1893631deba4189889d8fc3e43
Manh Pham <manhpham90vn@icloud.com>
2024-03-27 11:27:20 +0700

    sync

---
+++ install/cloud-sql-proxy.sh
@@ -0,0 +1,3 @@
+curl -o cloud-sql-proxy https://storage.googleapis.com/cloud-sql-connectors/cloud-sql-proxy/v2.10.1/cloud-sql-proxy.linux.amd64
+chmod +x cloud-sql-proxy
+sudo mv cloud-sql-proxy /usr/local/bin
